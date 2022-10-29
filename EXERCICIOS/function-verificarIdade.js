616898bbafd78d04bab760ee228a2ad7defafd75
karolina <poopinthegarden@gmail.com>
2022-10-29 20:07:43 -0300

    Create function-verificarIdade.js

---
+++ EXERCICIOS/function-verificarIdade.js
@@ -0,0 +1,10 @@
+/* Escreva uma função que diga se você é maior ou menor de idade*/
+
+function verificarIdade (idade){
+if (idade <=18){
+    console.log('Menor de Idade')
+    } else {
+    console.log('Maior de Idade')
+    }
+}
+verificarIdade(22)
