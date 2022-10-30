a4f9a607e4e2813bfea8d316332b52629617e8dc
karolina <poopinthegarden@gmail.com>
2022-10-30 06:52:30 -0300

    Update carros.js

--- EXERCICIOS DE OBJETOS E CLASSES/carros.js
+++ EXERCICIOS DE OBJETOS E CLASSES/carros.js
@@ -4,3 +4,21 @@ Crie um metodo que dado a quantidade de km e o preço
 do gasto em reai para realizar este percurso.
 */
 
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKM;
+
+    constructor(marca, cor, gastoMedioPorKM){
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKM = gastoMedioPorKM;
+    }
+
+calcularGastoPorPercurso(distanciaEmKm, precoCombustivel) {
+    return distanciaEmKm * this.gastoMedioPorKM * precoCombustivel;
+    }
+}
+
+const kwid = new Carro('Renault', 'Preto', 1/10);
+console.log(kwid.calcularGastoPorPercurso(70/5))
