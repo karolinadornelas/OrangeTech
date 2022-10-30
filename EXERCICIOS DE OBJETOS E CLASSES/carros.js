d74bb80f04e50c35ac2cef40a8c8f8901664dcd7
karolina <poopinthegarden@gmail.com>
2022-10-30 06:13:26 -0300

    Add files via upload

---
+++ EXERCICIOS DE OBJETOS E CLASSES/carros.js
@@ -0,0 +1,6 @@
+/*Crie uma classe para representar carros.
+Os carros possuem uma marca, uma cor e um gasto médio de combustivel.
+Crie um metodo que dado a quantidade de km e o preço
+do gasto em reai para realizar este percurso.
+*/
+
