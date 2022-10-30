f001c520525c44f7ac269fb513574af5f8b5e667
karolina <poopinthegarden@gmail.com>
2022-10-30 09:50:11 -0300

    Update tabuada.js

--- EXERCICIOS DE LISTAS/tabuada.js
+++ EXERCICIOS DE LISTAS/tabuada.js
@@ -1,12 +1,5 @@
 /*Crie um programa que dado um número imprima a sua tabuada.*/
-
-const tabuadaTres = [];
-
-    for (let i = 0; i <= 31; i++) {
-        const numeroDivisor = i % 3 === 0;
-        if (numeroDivisor) {
-            tabuadaTres.push(i);
-        }
-    }
-
-    console.log(tabuadaTres);
+const tabuada = 3;
+for (let i= 1; i <=3; i++){
+    console.log(i * tabuada)
+}
