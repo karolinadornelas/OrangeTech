5d58c8d7e96be0b74b3325e8ae10beaa50cc0a69
karolina <poopinthegarden@gmail.com>
2022-10-30 21:52:17 -0300

    Create login.js

---
+++ JavaScript/IF-ELSE/login.js
@@ -0,0 +1,10 @@
+/* Verificar as informações de username e password do usuário em uma estrutura condicional */
+
+var username = "PandaBalofo";
+var password = "123456";
+
+if (username == "PandaBaolofo" && password == "123456") {
+    console.log("Usuário autenticado");
+} else {
+    console.log("Login e/ou senha incorrretos");
+}
