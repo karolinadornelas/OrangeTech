1c236dccd10774c79a627e14d263be735be9e537
karolina <poopinthegarden@gmail.com>
2022-11-02 16:52:40 -0300

    Update listas-nome.js

--- JavaScript/EXERCICIOS DE ARRAYS/listas-nome.js
+++ JavaScript/EXERCICIOS DE ARRAYS/listas-nome.js
@@ -1,2 +1,5 @@
  /*Faça um programa que dado uma lista de nomes imprima os nomes que começem com a letra "V".*/
 
+var nomes = ['Kaneki', 'Kazehaya', 'Karolina', 'Yoshida', 'Vegeta'];
+var nomesComV = nomes.filter(nome => nome.charAt(0) == "V" );
+console.log(nomesComV);
